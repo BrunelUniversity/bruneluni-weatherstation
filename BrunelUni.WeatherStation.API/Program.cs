fcd0f3ddb4ab58d45af595c154a503d09e9ebdc8
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-03 22:40:50 +0000

    added test endpoint - to check if i2c works

--- BrunelUni.WeatherStation.API/Program.cs
+++ BrunelUni.WeatherStation.API/Program.cs
@@ -1,11 +1,29 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Unosquare.WiringPi;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/temperature", () => new Random().Next());
-app.MapGet("/humidity", () => new Random().Next());
-app.MapGet( "/run", ( ) => "test" );
+app.MapGet("/test", ( ) =>
+{
+    try
+    {
+        var bus = new I2CBus( );
+        var device = bus.AddDevice( 0x38 );
+        Task.Delay( 500 );
+        var status = device.ReadAddressWord( 0x71 );
+        Task.Delay( 100 );
+        device.WriteAddressWord( 0xac, 51 );
+        Task.Delay( 1000 );
+        var data = device.ReadAddressWord( 0x71 );
+        return$"status {status} data {data}";
+    }
+    catch( Exception e )
+    {
+        return e.Message;
+    }
+});
 
 app.Run();
