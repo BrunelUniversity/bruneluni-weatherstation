8904ea17ca79674315b0a5edf09eba0e4df7df55
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-03 02:41:35 +0000

    temperature and humidty api change

--- BrunelUni.WeatherStation.API/Program.cs
+++ BrunelUni.WeatherStation.API/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/temperature", () => new { temprature=new Random().Next() });
-app.MapGet("/humidity", () => new { temp=new Random().Next() });
+app.MapGet("/temperature", () => new Random().Next());
+app.MapGet("/humidity", () => new Random().Next());
 
 app.Run();
