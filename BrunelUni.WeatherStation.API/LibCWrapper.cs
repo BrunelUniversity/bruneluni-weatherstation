28a6de14a0f70f48133c1fed7739e14a2b8c14e2
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-04 18:22:28 +0000

    changed char set for lib calls

--- BrunelUni.WeatherStation.API/LibCWrapper.cs
+++ BrunelUni.WeatherStation.API/LibCWrapper.cs
@@ -5,15 +5,15 @@ namespace BrunelUni.WeatherStation.API;
 [ LibWrapper( Name = "libc.so.6" ) ]
 internal static class LibCWrapper
 {
-    [ DllImport( "libc.so.6", EntryPoint = "open", CharSet = CharSet.Unicode, SetLastError = true )  ]
+    [ DllImport( "libc.so.6", EntryPoint = "open", SetLastError = true )  ]
     public static extern int Open( string fileName, int mode );
  
-    [ DllImport( "libc.so.6", EntryPoint = "ioctl", CharSet = CharSet.Unicode, SetLastError = true ) ]
+    [ DllImport( "libc.so.6", EntryPoint = "ioctl", SetLastError = true ) ]
     public static extern int Ioctl( int fd, int request, int data );
  
-    [ DllImport( "libc.so.6", EntryPoint = "read", CharSet = CharSet.Unicode, SetLastError = true ) ]
+    [ DllImport( "libc.so.6", EntryPoint = "read", SetLastError = true ) ]
     public static extern int Read( int handle, byte[] data, int length );
 
-    [ DllImport( "libc.so.6", EntryPoint = "write", CharSet = CharSet.Unicode, SetLastError = true ) ]
+    [ DllImport( "libc.so.6", EntryPoint = "write", SetLastError = true ) ]
     public static extern int Write( int handle, byte[] data, int length );
 }
