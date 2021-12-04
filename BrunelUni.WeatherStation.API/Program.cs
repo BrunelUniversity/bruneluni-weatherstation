ab8c51f4a61aee00a1b6f5cb6783b9ecb047c001
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-04 22:22:10 +0000

    dont catch exception in api

--- BrunelUni.WeatherStation.API/Program.cs
+++ BrunelUni.WeatherStation.API/Program.cs
@@ -51,52 +51,45 @@ int read( int handle, byte [ ] data, int length ) => loadFunction<Read, LibCWrap
 
 int write( int handle, byte [ ] data, int length ) => loadFunction<Write, LibCWrapper>( ).Invoke( handle, data, length );
 
+
 app.MapGet("/test", ( ) =>
 {
-    try
+    var i2CBushandle = open( "/dev/i2c-1", openReadWrite );
+
+    const int registerAddress = 0x38;
+    var deviceReturnCode = ioctl( i2CBushandle, i2CSlave, registerAddress );
+
+    Task.Delay( 500 );
+
+    var writeData = new byte [ ] { 0xac, 0x33, 0x00 };
+
+    write( i2CBushandle, writeData, writeData.Length );
+    Task.Delay( 100 );
+
+    var secondReadBytes = new byte[ ] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        
+    read( i2CBushandle, secondReadBytes, secondReadBytes.Length );
+        
+    var tempRaw = new [ ]
     {
-        var i2CBushandle = open( "/dev/i2c-1", openReadWrite );
+        ( secondReadBytes[ 3 ] & 0x0F ) << 16,
+        secondReadBytes[ 4 ] << 8,
+        secondReadBytes[ 5 ]
+    }.Sum();
 
-        const int registerAddress = 0x38;
-        var deviceReturnCode = ioctl( i2CBushandle, i2CSlave, registerAddress );
-
-        Task.Delay( 500 );
-
-        var writeData = new byte [ ] { 0xac, 0x33, 0x00 };
-
-        write( i2CBushandle, writeData, writeData.Length );
-        Task.Delay( 100 );
-
-        var secondReadBytes = new byte[ ] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-        
-        read( i2CBushandle, secondReadBytes, secondReadBytes.Length );
-        
-        var tempRaw = new [ ]
-        {
-            ( secondReadBytes[ 3 ] & 0x0F ) << 16,
-            secondReadBytes[ 4 ] << 8,
-            secondReadBytes[ 5 ]
-        }.Sum();
-
-        var humRaw = new [ ]
-        {
-            secondReadBytes[ 1 ] << 12,
-            secondReadBytes[ 2 ] << 4,
-            ( secondReadBytes[ 3 ] & 0xF0 ) >> 4
-        }.Sum();
-
-        var returnVal = secondReadBytes.Skip( 1 ).Aggregate( "", ( current, b ) => current + $"byte{Array.IndexOf( secondReadBytes, b )}: {b} " );
-
-        var temperature = ( ( tempRaw / Math.Pow( 2, 20 ) ) * ( 200 ) ) - 50;
-
-        var humidity = ( humRaw / Math.Pow( 2, 20 ) ) * ( 100 );
-        
-        return $"{temperature} {humidity}";
-    }
-    catch( Exception e )
+    var humRaw = new [ ]
     {
-        return e.Message;
-    }
+        secondReadBytes[ 1 ] << 12,
+        secondReadBytes[ 2 ] << 4,
+        ( secondReadBytes[ 3 ] & 0xF0 ) >> 4
+    }.Sum();
+
+    var returnVal = secondReadBytes.Skip( 1 ).Aggregate( "", ( current, b ) => current + $"byte{Array.IndexOf( secondReadBytes, b )}: {b} " );
+
+    var temperature = ( ( tempRaw / Math.Pow( 2, 20 ) ) * ( 200 ) ) - 50;
+
+    var humidity = ( humRaw / Math.Pow( 2, 20 ) ) * ( 100 );
+        
+    return $"{temperature} {humidity}";
 });
-
 app.Run();
