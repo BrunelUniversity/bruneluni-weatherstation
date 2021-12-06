28bd0c88892705c98e9599ecc4f89a9371ff690c
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-06 03:27:10 +0000

    bumped version of aidan common utils for logging

--- BrunelUni.WeatherStation.API/Program.cs
+++ BrunelUni.WeatherStation.API/Program.cs
@@ -1,10 +1,10 @@
 using System;
+using System.Threading.Tasks;
 using Aidan.Common.Core.Interfaces.Contract;
 using BrunelUni.WeatherStation.Core.Interfaces.Contract;
 using BrunelUni.WeatherStation.Crosscutting.DIModule;
 using BrunelUni.WeatherStation.DIModule;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder( args );
 builder.Services
@@ -12,17 +12,24 @@ builder.Services
     .BindWeatherStationServices( );
 var app = builder.Build();
 
-//TODO: upgrade common to net6 and run all initialize classes
-app.Services.GetService<IPollingTemperatureStateService>( ).Initialize( );
-app.Services.GetService<ITemperatureEventState>( ).ValueChangedEvent += ( ) => Console.WriteLine( "state changed" );
+app.MapGet( "/init", ( IPollingTemperatureStateService pollingTemperatureStateService, ITemperatureEventState temperatureEventState ) =>
+{
+    //TODO: upgrade common to net6 and run all initialize classes
+    pollingTemperatureStateService.Initialize( );
+    temperatureEventState.ValueChangedEvent += ( ) => Console.WriteLine( "state changed" );
+    Task.Delay( 10000 );
+    return new
+    {
+        message = "success"
+    };
+} );
 
 app.MapGet( "/temperature/current",
-    ( IDHT20Service dht20Service, IDateTimeAdapter dateTimeAdapter ) => new
-    {
-        temperature = dht20Service.ReadTemperature( ).Value,
-        time = dateTimeAdapter.Now( )
-    } );
-
+        ( IDHT20Service dht20Service, IDateTimeAdapter dateTimeAdapter ) => new
+        {
+            temperature = dht20Service.ReadTemperature( ).Value,
+            time = dateTimeAdapter.Now( )
+        } );
 app.MapGet("/humidity/current",
     ( IDHT20Service dht20Service, IDateTimeAdapter dateTimeAdapter ) => new
     {
