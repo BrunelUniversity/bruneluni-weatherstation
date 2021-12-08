0071e127486de9e9c1155bb33a692d0f0ad0a5c2
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-08 13:48:25 +0000

    changed resolution of temperature capture to 0.5 a c

--- BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
+++ BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
@@ -26,7 +26,7 @@ public class TemperatureChangesCondition : BaseEventCondition, ITemperatureChang
         {
             return true;
         }
-        return Math.Abs( temperatureEventState.Value - latestValue.Celsius ) > 1;
+        return Math.Abs( temperatureEventState.Value - latestValue.Celsius ) > 0.5;
     } )
     {
     }
