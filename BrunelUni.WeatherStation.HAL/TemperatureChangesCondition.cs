f4d6d6a1fb62f37b11d0944cebc915f35fd46800
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-08 02:13:08 +0000

    add logging

--- BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
+++ BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Aidan.Common.Core.Interfaces.Contract;
 using Aidan.Common.Utils.EventDriven;
 using BrunelUni.WeatherStation.Core.Interfaces.Contract;
 
@@ -7,10 +8,13 @@ namespace BrunelUni.WeatherStation.HAL;
 public class TemperatureChangesCondition : BaseEventCondition, ITemperatureChangesCondition
 {
     public TemperatureChangesCondition( ITemperatureEventState temperatureEventState,
-        ITemperatureRepository temperatureRepository ) : base( ( ) =>
+        ITemperatureRepository temperatureRepository,
+        ILoggerAdapter<ITemperatureChangesCondition> loggerAdapter ) : base( ( ) =>
     {
         var latestValue = temperatureRepository.GetLatest( ).Value.Celsius;
-        return latestValue == null ? Math.Abs( temperatureEventState.Value - latestValue ) > 1 : true;
+        loggerAdapter.LogInfo( $"latest value is {latestValue}" );
+        loggerAdapter.LogInfo( $"current value is {temperatureEventState.Value}" );
+        return latestValue == null ? true : Math.Abs( temperatureEventState.Value - latestValue ) > 1;
     } )
     {
     }
