91503c289aaa71e6102cf250ce4f92a08ea0f318
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-10 12:14:48 +0000

    capture more temperature changes, capture less humidity changes

--- BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
+++ BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
@@ -26,7 +26,7 @@ public class TemperatureChangesCondition : BaseEventCondition, ITemperatureChang
         {
             return true;
         }
-        return Math.Abs( temperatureEventState.Value - latestValue.Celsius ) > 0.17;
+        return Math.Abs( temperatureEventState.Value - latestValue.Celsius ) > 0.15;
     } )
     {
     }
