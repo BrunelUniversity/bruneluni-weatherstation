4a0587a7e0a196be70d9dcea5a41b40ddecddc87
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-10 13:06:07 +0000

    increased temperature changes diff ever so slightly again

--- BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
+++ BrunelUni.WeatherStation.HAL/TemperatureChangesCondition.cs
@@ -26,7 +26,7 @@ public class TemperatureChangesCondition : BaseEventCondition, ITemperatureChang
         {
             return true;
         }
-        return Math.Abs( temperatureEventState.Value - latestValue.Celsius ) > 0.15;
+        return Math.Abs( temperatureEventState.Value - latestValue.Celsius ) > 0.175;
     } )
     {
     }
