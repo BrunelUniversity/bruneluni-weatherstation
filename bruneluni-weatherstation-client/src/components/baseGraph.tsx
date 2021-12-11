92867da5ca29eb35d8659f44011d106627d75d38
Aidan Gannon <63330150+aidangannon@users.noreply.github.com>
2021-12-11 17:31:50 +0000

    update graph to not display point

--- bruneluni-weatherstation-client/src/components/baseGraph.tsx
+++ bruneluni-weatherstation-client/src/components/baseGraph.tsx
@@ -45,7 +45,6 @@ const BaseGraph = ( props: { readings: BaseReading[], title: string } ): JSX.Ele
                 pointHoverBackgroundColor: '#faf7f7',
                 pointHoverBorderColor: '#faf7f7',
                 pointHoverBorderWidth: 1,
-                pointBorderWidth: 0.5,
                 pointHoverRadius: 5,
                 pointRadius: 2,
                 pointHitRadius: 10,
